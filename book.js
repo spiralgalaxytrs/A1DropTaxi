3150e6cf6f9f5ea626926836a022b0144adfd2e2
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2021-03-31 23:26:48 +0530

    Update book.js

--- book.js
+++ book.js
@@ -76,3 +76,41 @@
 
     });
   }
+   var strText = document.getElementById("name").value;
+        var strText1 = document.getElementById("p").value;
+        var strText2 = document.getElementById("location-1").value;
+        var strText3 = document.getElementById("location-2").value;
+        var strText4 = document.getElementById("pickupdate").value;
+        var strText6 = document.getElementById("pickuptime").value;
+        var strText7 = document.getElementById("cars").value;
+        var strText8 = document.getElementById("ser").value;
+        var rate;
+        if((strText7=="Sedan") && (strText8=="One-Way"))
+        {rate=sedano;}
+        else if((strText7=="Sedan") && (strText8="Round"))
+        {rate=sedanr;}
+        else if((strText7=="SUV") && (strText8=="One-Way"))
+        {rate=suvo;}
+        else if((strText7=="SUV") && (strText8=="Round"))
+        {rate=suvr;}
+        else if(strText7=="Traveller")
+        {rate=traveller;}
+        var result = 'Customer Name:  ' + strText + '%0APhone Number: ' + strText1 +'%0APickup Location: ' + strText2+  '%0ADrop Location: ' + strText3 + '%0APickup Date: ' + strText4 + '%0APickup Time: ' + strText6 +'%0AService: ' + strText8 + '%0ACars: ' + strText7+"%0APrice:"+rate;
+        var finalMsg = encodeURI(result);
+        document.getElementById("bookingForm").addEventListener("submit", (e) => {
+            e.preventDefault();
+            if (strText == "" || strText1 == "" || strText2 == "" || strText3 == "" || strText4 == "" || strText7 == "" || strText6 == "") {
+                console.log('error');
+                alert("Verfy the Phone Number");
+            } else {
+              
+            const request = new XMLHttpRequest();
+        const url = 'https://api.telegram.org/bot1277549383:AAGnKv4XGhOAG08Qg7ewe1mLVyXFiISPJ8o/sendMessage?chat_id=-1001479118068&text='+result;
+
+request.open("post", url);
+
+request.send();
+            window.open('https://intercityriders.com/bookingSuccess.html', '_self');
+        }
+
+});
