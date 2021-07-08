2e48f1bfe735d4f638c67b7c33d340fb95d73a9d
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2021-07-08 19:54:36 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ function calcRoute() {
             sedanr=2*((n*11)+175)+" - "+2*((n*11)+250);
             suvo=((n*18)+350)+" - "+((n*18)+500);
             suvr=2*((n*14)+175)+" - "+2*((n*14)+250);
-            traveller=(2*((n*18)+350))+" - "+(2*((n*18)+500));
+            traveller=(2*((n*18)+175))+" - "+(2*((n*18)+250));
            
           //  $("#output").html("<div class='result-table'> Driving distance: " + result.routes[0].legs[0].distance.text + ".<br />SUV Rate: ₹" +((n)*12)+"-"+((n)*15)+".<br />Sedan Rate: ₹" +((n)*8)+"-"+((n)*11)+".<br />Hatchback Rate: ₹" +((n)*16)+"-"+((n)*19)+ ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".</div>");
             //document.getElementById("output").style.display = "block";
